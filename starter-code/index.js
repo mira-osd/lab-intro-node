342ab461bdeba84e5db76b298fb10430cd616b3c
Mira <myrahosd@hotmail.fr>
2019-11-19 21:18:09 +0100

    jusqu'à max()

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,8 +1,32 @@
 class SortedList {
-    constructor() {}
-    add(item) {}
-    get(pos) {}
-    max() {}
+    constructor(items, length) {
+      this.items = [];
+      this.length = 0;
+    }
+    add(item) {
+      this.items.push(item);
+      this.items.sort(function (a, b){
+        if(a < b) return -1;
+        if(a > b) return 1;
+        if(a === b) return 0;
+      })
+      this.length++
+    }
+    get(pos) {
+      if(pos <= this.length){
+        return this.items[pos];
+      } else {
+        throw new Error("OutOfBounds");
+      }
+    }
+    // à changer le max()
+    max(high) { 
+      if(high === this.items[pos]){ 
+        return this.items[high];
+      } else {
+        throw new Error("EmptySortedList");
+      }
+    }
     min() {}
     average() {}
     sum() {}
